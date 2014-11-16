d4ece8467ec7c508467264bedbfc868bf2f719ca
Eric Lu <penlume@gmail.com>
2014-11-15 22:43:30 -0500

    added further experiments with transition details to the background page

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+chrome.webNavigation.onCommitted.addListener(function(details) {
 	console.log("navigation detected! url is ".concat(details.url));
-})
+	console.log("transition type: ".concat(details.transitionType));
+	console.log("transition qualifiers: ".concat(details.transitionQualifiers));
+});
