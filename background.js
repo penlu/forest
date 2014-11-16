187bd0810a804d52380076f854c695214364ed1d
Eric Lu <penlume@gmail.com>
2014-11-15 23:01:37 -0500

    manifest requests storage permissions; now classifying navigation types

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
 chrome.webNavigation.onCommitted.addListener(function(details) {
 	console.log("navigation detected! url is ".concat(details.url));
-	console.log("transition type: ".concat(details.transitionType));
-	console.log("transition qualifiers: ".concat(details.transitionQualifiers));
+
+	var type = details.transitionType;
+	if (["link", "manual_subframe", "form_submit"].indexOf(type) != -1) { // user clicked link
+		console.log("clicked link!");
+	} else if (["typed", "generated", "keyword", "keyword_generated"].indexOf(type) != -1) { // user typed something into the box
+		console.log("typed into box!");
+	} else {
+		console.log("unclassified action");
+	}
 });
