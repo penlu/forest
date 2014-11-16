fe3bf3be91a1ec11c9d304324479b0ea494caac8
cyc37 <cyc37@cornell.edu>
2014-11-16 03:15:39 -0500

    reformatted print statements

--- background.js
+++ background.js
@@ -1,12 +1,22 @@
-chrome.webNavigation.onCommitted.addListener(function(details) {
-	console.log("navigation detected! url is ".concat(details.url));
+chrome.webNavigation.onCommitted.addListener(
+	function(details)
+	{
+		console.log("============================== NEW NODE ==============================\n")
+		console.log("URL: ".concat(details.url));
 
-	var type = details.transitionType;
-	if (["link", "manual_subframe", "form_submit"].indexOf(type) != -1) { // user clicked link
-		console.log("clicked link!");
-	} else if (["typed", "generated", "keyword", "keyword_generated"].indexOf(type) != -1) { // user typed something into the box
-		console.log("typed into box!");
-	} else {
-		console.log("unclassified action");
+		var type = details.transitionType;
+
+		if (["link", "manual_subframe", "form_submit"].indexOf(type) != -1)		// user clicked link
+		{
+			console.log("Navigation: CLICK");
+		}
+		else if (["typed", "generated", "keyword", "keyword_generated"].indexOf(type) != -1)	// user typed something into the box
+		{
+			console.log("Navigation: TYPE");
+		}
+		else
+		{
+			console.log("Navigation: UNCLASSIFIED");
+		}
 	}
-});
+);
