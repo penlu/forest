de1fbfeec698f6522fee3761095522930e450538
Eric Lu <penlume@gmail.com>
2014-11-16 08:25:17 -0500

    stores event info and indicates user activity when appropriate

--- background.js
+++ background.js
@@ -9,19 +9,13 @@ function saveTransition(orig, dest) {
 
 }
 
-// called after piece of user data has been contributed
-// if all user data has been aggregated, then 
-function handleUserAction(tabId) {
-	console.log("Tab ID: ".concat(tabId))
-}
-
 // cache storage for navigation events
 var navigationEventCache = {}
 
 chrome.webNavigation.onCommitted.addListener(
 	function(details)
 	{
-		console.log("============================== NEW NODE ==============================\n")
+		console.log("============================== NEW NODE ==============================")
 		console.log("URL: ".concat(details.url))
 
 		var type = details.transitionType
@@ -40,6 +34,12 @@ chrome.webNavigation.onCommitted.addListener(
 			console.log("Navigation: IGNORE")
 		}
 
+		// TODO save this stuff conditionally on transitionQualifiers
+		navigationEventCache[details.tabId] = {
+			"transitionType": details.transitionType,
+			"transitionQualifiers": details.transitionQualifiers
+		}
+
 		handleUserAction(details.tabId)
 	}
 )
@@ -49,7 +49,7 @@ var tabEventCache = {}
 
 chrome.tabs.onUpdated.addListener(
 	function(tabId, changeInfo, tab) {
-		console.log("================== TAB UPDATED ==================")
+		console.log("~~~~~~~~~~~~~~~~~~ TAB UPDATED ~~~~~~~~~~~~~~~~~~")
 		console.log("Tab URL: ".concat(changeInfo.url))
 
 		if (typeof changeInfo.url !== "undefined") {
@@ -63,24 +63,70 @@ chrome.tabs.onUpdated.addListener(
 	}
 )
 
-chrome.tabs.onReplaced.addListener(
-	function(addedTabId, removedTabId) {
-		console.log("================== TAB REPLACED ==================")
-		console.log("Added Tab: ".concat(addedTabId))
-		console.log("Removed Tab: ".concat(removedTabId))
+chrome.tabs.onCreated.addListener(
+	function(tab) {
+		console.log("~~~~~~~~~~~~~~~~~~ TAB CREATED ~~~~~~~~~~~~~~~~~~")
+		console.log("New Tab ID: ".concat(tab.tabId))
+		console.log("New Tab URL: ".concat(tab.url))
 	}
 )
 
-chrome.tabs.onRemoved.addListener(
-	function (tabId)
-	{
-		var thisTab = tabId
+chrome.tabs.onReplaced.addListener(
+	function(addedTabId, removedTabId) {
+		console.log("~~~~~~~~~~~~~~~~~~ TAB REPLACED ~~~~~~~~~~~~~~~~~~")
+		console.log("Added Tab: ".concat(addedTabId))
+		console.log("Removed Tab: ".concat(removedTabId))
+
+		var addedTabInfo = chrome.tabs.get(addedTabId, function(tab) {
+			console.log("Added Tab URL: ".concat(tab.url))
+			if (typeof tab.url !== "undefined") {
+				tabEventCache[addedTabId] = {
+					"url": tab.url
+				}
+			}
+		})
+		
+		tabRemoved(removedTabId)
+
+		handleUserAction(addedTabId)
 	}
 )
+
+// removes events from caches when tab is removed
+function tabRemoved(tabId)
+{
+	console.log("~~~~~~~~~~~~~~~~~~ TAB REMOVED ~~~~~~~~~~~~~~~~~~")
+	console.log("Tab Removed: ".concat(tabId))
+	delete navigationEventCache[tabId]
+	delete tabEventCache[tabId]
+}
+chrome.tabs.onRemoved.addListener(tabRemoved)
+
+// keep track of current active tab
+var activeTab = -1
 
 chrome.tabs.onActivated.addListener(
 	function (activeInfo)
 	{
-		var thisTab = activeInfo.tabId
+		activeTab = activeInfo.tabId
 	}
 )
+
+// called after piece of user data has been contributed
+// if all user data has been aggregated, then perform navigation saving etc
+function handleUserAction(tabId) {
+	console.log("Handle user action call, tab ID: ".concat(tabId))
+	if (typeof navigationEventCache[tabId] !== "undefined" && typeof tabEventCache[tabId] !== "undefined") {
+		console.log("!!!!!!!!!!!! HANDLING NOW !!!!!!!!!!!!")
+		var userAction = {
+			"url": tabEventCache[tabId].url,
+			"transitionType": navigationEventCache[tabId].transitionType,
+			"transitionQualifiers": navigationEventCache[tabId].transitionQualifiers
+		}
+		console.log("Handled URL: ".concat(userAction.url))
+		console.log("Handled Transition Type: ".concat(userAction.transitionType))
+		console.log("Handled Transition Qualifiers: ".concat(userAction.transitionQualifiers))
+		delete navigationEventCache[tabId]
+		delete tabEventCache[tabId]
+	}
+}
